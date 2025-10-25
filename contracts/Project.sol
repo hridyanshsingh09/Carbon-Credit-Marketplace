89aab763527d89e0985670070bf8a3c947163b08
hridyanshsingh09 <hridyanshencorecalm@gmail.com>
2025-10-25 16:39:47 +0530

    Update Project.sol
    Wrote a small contract showing the description and the name of the project

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,7 +1,20 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
 
-pragma solidity >=0.8.2 <0.9.0;
+contract Identity {
+    string Project;
+    string Description;
 
-contract Carbon-Credit-Marketplace {
+    constructor() {
+        Project = "Carbon-Credit-Marketplace";
+        Description = unicode"A Carbon Credit Marketplace is a blockchain-based platform that enables transparent trading of carbon credits — digital tokens representing a unit of reduced or removed CO₂ emissions. It connects companies, investors, and environmental projects to buy, sell, and verify carbon offsets securely and efficiently.";
+    }
+    
+    function showProject() public view returns (string memory) {
+        return Project;
+    }
+    
+    function showDescription() public view returns (string memory) {
+        return Description; 
+    }
 }
-
